d4acc10dab8668b52b37e0cbd0d9b9b448efd81f
Jessica Dao <xjessicadao@gmail.com>
2021-02-10 00:35:56 -0800

    added server

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/werkout", { useNewUrlParser: true });
+
+
+app.post("/exercise", ({ body }, res) => {
+    db.Exercise.create(body)
+        .then(({ _id }) => db.Workout.findOneAndUpdate({}, 
+            { $push: { exercises: _id } }, { new: true }))
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.post("/workout", (req, res) => {
+    db.Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.get("/all", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
